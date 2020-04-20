0d08ea0743de8429d51dfc5142ba86d5063ffbe2
jonas27 <joE@12345>
2020-04-20 06:38:31 +0200

    add tests

--- pushDocker.sh
+++ pushDocker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=v0.1.0
+version=latest
 docker build . -t jonas27test/goserver:$version
 # docker run -p 8080:8080 -p 4443:4443 -v $HOME/repos/goserver/cert:/cert -v $HOME/repos/goserver/static:/static jonas27test/goserver:v0.0.1 
 docker push jonas27test/goserver:$version
